2710c79b5ddd1ab41aed28501ac17f39a072ff8f
Max Romanenko <romanenkomax.ua@gmail.com>
2018-07-23 14:16:39 +0300

    Project ready for deploy

--- src/main/java/ua/danit/dao/ChatDAOtoDB.java
+++ src/main/java/ua/danit/dao/ChatDAOtoDB.java
@@ -1,14 +1,10 @@
 package ua.danit.dao;
 
-import com.google.common.io.CharStreams;
 import ua.danit.model.Chat;
-import ua.danit.model.User;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ChatDAOtoDB extends AbstractDAOtoDB<Chat> {
